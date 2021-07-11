d4d8a6bf3885efe6ae546d440ffd94dc6bce2594
FannyLim001 <fanny20ti@mahasiswa.pcr.ac.id>
2021-07-11 17:31:30 +0700

    detail produk dari db

--- resources/views/v_detail_product.blade.php
+++ resources/views/v_detail_product.blade.php
@@ -72,14 +72,19 @@
     <!-- ***** Header Area End ***** -->
 
     <!-- ***** Call to Action Start ***** -->
-    <section class="section section-bg" id="call-to-action" style="background-image: url(template/user/assets/images/banner-image-1-1920x500.jpg)">
+    <section class="section section-bg" id="call-to-action" style="background-image: url(template/user/assets/images/brochures.jpg)">
         <div class="container">
             <div class="row">
                 <div class="col-lg-10 offset-lg-1">
                     <div class="cta-content">
                         <br>
                         <br>
-                        <h2><small><del>$12 999</del></small> <em>$11 779</em></h2>
+                        <h2> <em> Rp.
+                                @foreach($barang as $b)
+                                {{$b->harga_barang}}
+                                @endforeach
+                            </em>
+                        </h2>
                         <p>Lorem ipsum dolor sit amet, consectetur.</p>
                     </div>
                 </div>
